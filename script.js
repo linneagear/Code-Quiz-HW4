7aee2951d64785f2b65d315742fdad86ed0db1b2
Linnea <linneagear@gmail.com>
2020-04-21 18:38:13 -0700

    Added questions and answers in an object

---
+++ script.js
@@ -0,0 +1,43 @@
+// have multiple questions in object
+var questions = [
+    {
+        question: "Which of these is NOT a programming language?",
+        choiceA: "Java",
+        choiceB: "Banana",
+        choiceC: "Ruby",
+        choiceD: "Python",
+        correct:  "B",
+    },
+    {
+        question: "What are people who write computer code called?",
+        choiceA: "Cryptographers",
+        choiceB: "Professors",
+        choiceC: "Programmers",
+        choiceD: "Manufacturers",
+        correct:  "C",
+    },
+    {
+        question: "What word describes the set of instructions that computers need to work?",
+        choiceA: "Agenda",
+        choiceB: "Synopsis",
+        choiceC: "Blueprint",
+        choiceD: "Program",
+        correct:  "D",
+    },
+    {
+        question: "What is computer coding?",
+        choiceA: "A radio show",
+        choiceB: "Telling a computer what to do",
+        choiceC: "A TV show",
+        choiceD: "A list of functions",
+        correct:  "B",
+    },
+    {
+        question: "Which of these does NOT run using a computer program?",
+        choiceA: "Space rockets",
+        choiceB: "Bikes",
+        choiceC: "Cars",
+        choiceD: "Trains",
+        correct:  "B",
+    }
+]
