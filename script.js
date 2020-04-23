e74ae01679e63694a7f8a24e4f5b5cfe1162f16e
Linnea <linneagear@gmail.com>
2020-04-22 20:36:06 -0700

    Questions and choices appear on screen

--- script.js
+++ script.js
@@ -2,15 +2,11 @@
 var start = document.getElementById("startPage");
 var quiz = document.getElementById("quiz");
 var question = document.getElementById("question");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
-var submit = document.getElementById("submit");
-var clearHighscores = document.getElementById("clearHighscores");
-var playAgain = document.getElementById("playAgain");
-var scoreSpan = document.getElementById("score")
-// var next = document.getElementById("next")
+var choiceA = document.getElementById('A');
+var choiceB = document.getElementById('B');
+var choiceC = document.getElementById('C');
+var choiceD = document.getElementById('D');
+var next = document.getElementById("next");
 
 // have multiple questions in object
 var questions = [
@@ -54,24 +50,24 @@ var questions = [
         choiceD: "Trains",
         correct:  "B",
     }
-]
+];
 
 // value variables, must be AFTER questions
 var lastQuestion = questions.length - 1;
-var nextQuestion = 0;
+var nextQuestionIndex = 0;
 var score = 0;
 
 function nextQuestion() {
-    // index of next question, grabbed from the question object
-    var q = questions[nextQuestion];
+    var q = questions[nextQuestionIndex];
     // replace the question/choice div with the appropriate question and choices
     question.innerHTML = q.question;
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD;
-    nextQuestion();
 }
+
+nextQuestion();
 
 function startQuiz() {
     event.preventDefault();
@@ -82,16 +78,6 @@ function startQuiz() {
     Timer();
     setTime();
 }
-
-// // if correct, display Correct!
-// function correctAnswer() {
-//     document.getElementById("correct").style.display = "Correct!";    
-// }
-
-// // if correct, display Inorrect!
-// function incorrectAnswer() {
-//     document.getElementById("incorrect").style.display = "Incorrect!";   
-// }
 
 var secondsLeft = 500;
 var timerInterval;
@@ -106,43 +92,63 @@ function Timer() {
     secondsLeft--;
 
     timeEl.textContent = secondsLeft;
-// if time is 0, clear time interval and go to score page
+    // if time is 0, clear time interval and go to score page
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
       setTimeout(renderScore, 1000);
     }
 }
 
-// function renderScore() {
-//     // display the last page of the quiz to show score
-//     quiz.style.display = "block";
-// }
-
 
 // run the user's answer through this function to then check using conditional statements
-// function checkAnswer(answer) {
-//     // if question is correct, then go to next question
-//     if(questions[nextQuestion].correct == answer) {
-//         score++
-//         correctAnswer();
-//     } else {
-//         incorrectAnswer();
-//     }
-//     // if not the last question, then continue with next question
-//     if(nextQuestion < lastQuestion) {
-//         // timer count = 0;
-//         // go to next question
-//         nextQuestion++;
-//         // render the question
-//         questionRender();
-//     } else {
-//         // stop the timer and show the score
-//         clearInterval(Timer)
-//         renderScore();
-//     }
-// }
+function checkAnswer(answer) {
+    // if question is correct, then go to next question
+    if(questions[nextQuestionIndex].correct === answer) {
+        score++
+        correctAnswer();
+    } else {
+        incorrectAnswer();
+        // timer decreases
+    }
+    // if not the last question, then continue with next question
+    if(nextQuestion < lastQuestion) {
+        // timer count = 0;
+        // go to next question
+        nextQuestionIndex++;
+        // ask the next question
+        nextQuestion;
+    } else {
+        // stop the timer and show the score
+        clearInterval(Timer)
+        finalScore();
+    }
+}
+
+// if correct, display Correct!
+function correctAnswer() {
+    document.getElementById("correct").style.display = "block";    
+}
+
+// if correct, display Inorrect!
+function incorrectAnswer() {
+    document.getElementById("incorrect").style.display = "block";   
+}
+
+// var submit = document.getElementById("submit");
+// var clearHighscores = document.getElementById("clearHighscores");
+// var playAgain = document.getElementById("playAgain");
+// var scoreSpan = document.getElementById("score");
+// var finalScorePage = document.getElementById("finalScores");
+
+// // function finalScore() {
+// //     // display the last page of the quiz to show score
+// //     finalScorePage.style.display = "block";
+// //          var finalScore = 
+
+// play again : refresh, back to first page
+
+// // }
 
 
 // add event listeners for all buttons
 start.addEventListener("click", startQuiz);
-// next.addEventListener("click", nextQuestion);
